dee18b12485d055722d10d708cd145988a27436b
Anders Holt Knudsen <andhk11@gmail.com>
2020-10-31 15:13:05 +0100

    added http POST request and validation

--- routes/courses.js
+++ routes/courses.js
@@ -1,20 +1,40 @@
-const express = require('express');
+const Joi = require("joi");
+const express = require("express");
 const router = express.Router();
 
 const courses = [
-    {id: 1, name: "course1"},
-    {id: 2, name: "course2"},
-    {id: 3, name: "course3"},
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 router.get("/", (req, res) => {
-    res.send(courses);
+  res.send(courses);
 });
 
 router.get("/:id", (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found.')
-    res.send(course);
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found.");
+  res.send(course);
+});
+
+router.post("/", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const validationResult = Joi.validate(req.body, schema);
+  if (validationResult.error) {
+    res.status(400).send(validationResult.error.details[0].message);
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
 });
 
 module.exports = router;
