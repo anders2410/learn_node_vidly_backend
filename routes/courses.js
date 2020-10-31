7e93beb7d21aced81f19ef246a502f948fb34791
Anders Holt Knudsen <andhk11@gmail.com>
2020-10-31 13:53:03 +0100

    added http GET request

---
+++ routes/courses.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const courses = [
+    {id: 1, name: "course1"},
+    {id: 2, name: "course2"},
+    {id: 3, name: "course3"},
+]
+
+router.get("/", (req, res) => {
+    res.send(courses);
+});
+
+router.get("/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found.')
+    res.send(course);
+});
+
+module.exports = router;
