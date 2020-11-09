f24e6e296f620513ddc0bc595d09ada15a2aa153
Anders Holt Knudsen <andhk11@gmail.com>
2020-11-09 23:25:13 +0100

    Started to add movies

---
+++ models/movie.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { genreSchema } = require("./genre");
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 5,
+    maxLength: 255,
+  },
+  genre: {
+    type: genreSchema,
+    required: true,
+  },
+  numberInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  dailyRentalRate: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+});
+
+const Movie = mongoose.model("movies", movieSchema);
+
+// Validating the Movie
+function validateMovie(movie) {
+  const schema = Joi.object({
+    title: Joi.string().min(1).max(100).required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().max(100).required(),
+    dailyRentalRate: Joi.number().required(),
+  });
+
+  return schema.validate(movie);
+}
+
+exports.Movie = Movie;
+exports.validate = validateMovie;
---
+++ routes/movies.js
@@ -0,0 +1,35 @@
+const { Movie, validate } = require("../models/movie.js");
+const { Genre } = require("../models/genre.js");
+const express = require("express");
+const router = express.Router();
+
+// HTTP-GET: Get all the genres
+router.get("/", async (req, res) => {
+  const movies = await Movie.find().sort("name");
+  res.send(movies);
+});
+
+// HTTP-POST: Posts a movie to the server
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(404).send("The genre with the given ID was not found.");
+  }
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
+
+  let movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+
+  await movie.save();
+});
+
+module.exports = router;
